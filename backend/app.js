845a810fb15ed3d1946d6bad17b43dcfbfd6da3e
Akhilesh Yadav <akhileshyadav3841@gmail.com>
2025-08-12 18:39:45 +0530

    commithai

--- backend/app.js
+++ backend/app.js
@@ -2,21 +2,20 @@ const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
-require("dotenv").config();
 
 const app = express();
 const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://musicfrontend-one.vercel.app" || "http://localhost:5173",
+    origin:"http://localhost:5173",
     methods: ["GET", "POST"],
   },
 });
 
 app.use(
   cors({
-    origin: "https://musicfrontend-one.vercel.app" || "http://localhost:5173",
+    origin:"http://localhost:5173",
   })
 );
 app.use(express.json());
@@ -199,7 +198,7 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT =5000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
